ffd7c522d9ba624343114b13c625353b28f9c156
ANDRES KWAN <andreskwan@gmail.com>
2014-05-17 14:32:50 -0500

    backbone - handlebars - template working

--- public/js/main.js
+++ public/js/main.js
@@ -6,16 +6,14 @@ Sfotipy.Song     = Backbone.Model.extend({});
 Sfotipy.SongView = Backbone.View.extend({
 	tagName  : 'li',
 	className: 'item border-bottom',
+	template: Handlebars.compile($("#song-template").html()),
 
 	//function para dibujar la vista 
 	render: function () {
-		var song   = this.model;
-		var name   = song.get('name');
-		var author = song.get('author');
- 
+		var html = this.template(this.model.toJSON());
  		//helper de jquery
  		//para ingresar contenido a la vista
-		this.$el.html("<span>"+ author +"</span> - <span>"+ name +"</span>")
+		this.$el.html(html);
 	}
 });
 //para ponerlo global
