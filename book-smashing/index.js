1a2bf04af9496121ed2bf40da036a1d23d13b11b
ANDRES KWAN <andreskwan@gmail.com>
2014-06-11 17:14:11 -0500

    Classes Prototype & Export

--- book-smashing/index.js
+++ book-smashing/index.js
@@ -1,5 +1,3 @@
-var a = require('./module_a.js');
-
-console.log(a.name);
-console.log(a.data);
-console.log(a.getPrivate());
+var Person = require('./person.js');
+var john = new Person('john');
+john.talk();
