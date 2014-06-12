66c955db1463fe0c575143929da723cd441dad8a
ANDRES KWAN <andreskwan@gmail.com>
2014-06-12 10:17:16 -0500

    EventEmitter

--- book-smashing/index.js
+++ book-smashing/index.js
@@ -1,3 +1,10 @@
-var Person = require('./person.js');
-var john = new Person('john');
-john.talk();
+//ch5 - event emitter
+var EventEmitter = require('events').EventEmitter;
+var a			 = new EventEmitter;
+
+a.on('event', function (){
+	console.log('event called');
+});
+
+//calls the event in the same file!!!
+a.emit('event');
